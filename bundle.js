a1f1d789e2343f3fde550a3cbf92a9e3a569a4de
PotHix <pothix@pothix.com>
2011-01-29 04:57:50 -0200

    Removing player tile that returned with merge hell

--- bundle.js
+++ bundle.js
@@ -6,7 +6,6 @@
     ["minion-ice","resources/minion-ice.png"],
     ["font","resources/font.png"],
     ["enemy01","resources/enemy01.png"],
-    ["player","resources/player_tile1.png"],
     ["map_background", "resources/map_background.png"]
   ],
 
