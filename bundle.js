9c52f81a0c179a08a3ec96795074bbcbdc6bbd90
PotHix <pothix@pothix.com>
2011-01-29 02:34:29 -0200

    Adding enemy image files

--- bundle.js
+++ bundle.js
@@ -1,6 +1,9 @@
 {
   // Images
   addImage:[
-    ["logo","resources/logo.png"]
+    ["logo","resources/logo.png"],
+    ["titan-ice","resources/titan-ice.png"],
+    ["minion-ice","resources/minion-ice.png"],
+    ["enemy01","resources/enemy01.png"]
   ]
 }
