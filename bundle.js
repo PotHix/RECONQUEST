460dc3df902df56807964d708825069c7efb2b58
agoncalves <agoncalves@ikwa.com.br>
2011-01-29 03:13:33 -0200

    Try solved problem game load

--- bundle.js
+++ bundle.js
@@ -4,9 +4,15 @@
     ["logo","resources/logo.png"],
     ["titan-ice","resources/titan-ice.png"],
     ["minion-ice","resources/minion-ice.png"],
+    ["font","resources/font.png"],
     ["enemy01","resources/enemy01.png"]
   ],
   addTiles:[
     {id:"enemy01", image:"enemy01", tileh:32, tilew:16, tilerow:1, gapx:0, gapy:0}
+  ],
+
+  addFont:[
+    {id:"small",image:"font",firstletter:" ",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:8}
   ]
+
 }
--- src/game.js
+++ src/game.js
@@ -1,11 +1,36 @@
 // ---
 // Copyright (c) 2011 PotHix, http://pothix.com/blog
 // ---
-
+var maingame;
 function main(){
-  var maingame;
   gbox.setGroups(['game']);
   maingame = gamecycle.createMaingame('game', 'game');
-
+ 
+  maingame.gameTitleIntroAnimation=function(reset) {
+  if (reset) {
+    toys.resetToy(this, 'rising');
+  }
+ 
+  gbox.blitFade(gbox.getBufferContext(),{ alpha: 1 });
+ 
+  toys.logos.linear(this, 'rising', {
+    image: 'logo',
+    sx:    gbox.getScreenW()/2-gbox.getImage('logo').width/2,
+    sy:    gbox.getScreenH(),
+    x:     gbox.getScreenW()/2-gbox.getImage('logo').width/2,
+    y:     20,
+    speed: 1
+  });
+  };
+  
+  maingame.pressStartIntroAnimation=function(reset) {
+    if (reset) {
+      toys.resetToy(this,"default-blinker");
+    } else {
+      toys.text.blink(this,"default-blinker",gbox.getBufferContext(),{font:"small",text:"PRESS Z TO START",valign:gbox.ALIGN_MIDDLE,halign:gbox.ALIGN_CENTER,dx:0,dy:Math.floor(gbox.getScreenH()/3),dw:gbox.getScreenW(),dh:Math.floor(gbox.getScreenH()/3)*2,blinkspeed:10});
+      return gbox.keyIsHit("a");
+      }
+  };
+ 
   gbox.go();
 }
