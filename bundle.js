2dc9a38d18e03aee573f7f1a07ec1ab026dd8781
PotHix <pothix@pothix.com>
2011-01-30 05:53:48 -0200

    Adding bars to the game

--- bundle.js
+++ bundle.js
@@ -5,6 +5,10 @@
     ["titan-ice","resources/titan-ice.png"],
     ["minion-ice","resources/minion-ice.png"],
     ["font","resources/font.png"],
+    ["health-frame","resources/bar_health-frame.png"],
+    ["energy-frame","resources/bar_energy-frame.png"],
+    ["health-fluid","resources/bar_health-fluid.png"],
+    ["energy-fluid","resources/bar_energy-fluid.png"],
     ["enemy01","resources/enemy01.png"],
     ["map_background", "resources/map_background.png"],
     ["player","resources/titan-ice.png"],
@@ -14,8 +18,12 @@
 
   // Tiles
   addTiles:[
-    {id:"enemy-tiles", image:"enemy01", tilew:16, tileh:32, tilerow:6, gapx:0, gapy:0},
-    {id:"spell-tile", image:"spell1", tilew:16, tileh:32, tilerow:1, gapx:0, gapy:0}
+    {id:"enemy-tiles",      image:"enemy01", tilew:16,  tileh:32, tilerow:6, gapx:0, gapy:0},
+    {id:"spell-tile",       image:"spell1",  tilew:16,  tileh:32, tilerow:1, gapx:0, gapy:0},
+    {id:"hud-health-frame", image:"health-frame",  tilew:221, tileh:23, tilerow:1, gapx:0, gapy:0},
+    {id:"hud-energy-frame", image:"energy-frame",  tilew:221, tileh:23, tilerow:1, gapx:0, gapy:0},
+    {id:"hud-health-fluid", image:"health-fluid",  tilew:194, tileh:9,  tilerow:1, gapx:0, gapy:0},
+    {id:"hud-energy-fluid", image:"energy-fluid",  tilew:194, tileh:9,  tilerow:1, gapx:0, gapy:0}
   ],
 
   // Fonts
