ef77756866801d9e0aa045b8f591c9d57ddd9f4d
PotHix <pothix@pothix.com>
2011-01-30 14:42:13 -0200

    Uncommenting energy bar. Let it go! :D

--- src/stage.js
+++ src/stage.js
@@ -10,9 +10,9 @@ var Stage = function()
             group: 'game',
             x: 0,  y: 0,
             w: 48, h: 48,
-            
+
             energy_counter: 0,
-            
+
             game_over: false,
 
             initialize: function() {
@@ -24,21 +24,21 @@ var Stage = function()
 
                 this.enemyGenerator = new EnemyGenerator;
                 this.enemyGenerator.init();
-                
-                /*this.energy = new EnergyBar;
-                this.energy.init();*/
+
+                this.energy = new EnergyBar;
+                this.energy.init();
 
                 this.health = new HealthBar;
                 this.health.init();
-                
-                
+
+
                 // Adding our platform (sure, it IS bad code. haven't you noticed yet?)
                 gbox.addObject({
                     group: "platform",
                     image: "platform",
-                    
+
                     x: 38, y: 275,
-                    
+
                     blit: function() {
                         gbox.blitAll(gbox.getBufferContext(), gbox.getImage(this.image), {dx:this.x, dy:this.y});
                     }
@@ -49,23 +49,23 @@ var Stage = function()
 		gbox.stopAudio("title_screen");
 		gbox.playAudio("bgm");
                 var enemies = this.enemyGenerator.object.enemies;
-                
+
                 if (this.energy_counter++ > 200)
                 {
                     if (maingame.hud.getValue("energy", "value") < 6)
                     {
                         maingame.hud.addValue("energy","value",1);
                     }
-                    
+
                     this.enemyGenerator.object.addEnemy();
-                    
+
                     this.energy_counter = 0;
                 }
-                
+
                 for (var i = 0; i < enemies.length; i++)
                 {
                     var e = enemies[i];
-                    
+
                     if (e.object.life <= 0)
                     {
                         this.enemyGenerator.object.spawn(e);
@@ -77,7 +77,7 @@ var Stage = function()
 			{
 			    maingame.hud.addValue("health","value",-1);
 			    this.enemyGenerator.object.spawn(e);
-			    
+
 			    if (maingame.hud.getValue("health", "value") < 2 && !this.game_over)
 			    {
 		                gbox.stopAudio("bgm");
