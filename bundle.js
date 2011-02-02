32ad1b1e613260865a4d148a71f0abe8113eadd2
PotHix <pothix@pothix.com>
2011-02-02 00:29:02 -0200

    Changing game zoom on mobile Changing sound type to bgmusic

--- bundle.js
+++ bundle.js
@@ -36,8 +36,8 @@
 
 	// Audio
 	addAudio:[
+		["bgm",["resources/audio/tlol-cave.mp3","resources/audio/tlol-cave.ogg"],{channel:"bgmusic",loop:true}],
 		["explosion",["resources/audio/megaexplosion.mp3","resources/audio/megaexplosion.ogg"],{channel:"sfx"}],
-		["title_screen",["resources/audio/solitude-boss.mp3","resources/audio/solitude-boss.ogg"],{channel:"sfx",loop:true}],
-		["bgm",["resources/audio/tlol-cave.mp3","resources/audio/tlol-cave.ogg"],{channel:"sfx",loop:true}]
+		["title_screen",["resources/audio/solitude-boss.mp3","resources/audio/solitude-boss.ogg"],{channel:"bgmusic",loop:true}]
 	]
 }
