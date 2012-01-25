dc0cc7a6f9cc08f00b7615d105f15d1da2955eb9
PotHix <pothix@pothix.com>
2012-01-25 21:41:33 -0200

    Removing all content. It will be available only on gh-pages branch.

--- audio-bundle.js
+++
@@ -1,8 +0,0 @@
-{
-	// Audio
-	addAudio:[
-		["bgm",["resources/audio/game-song.mp3","resources/audio/game-song.ogg"],{channel:"bgmusic",loop:true}],
-		["title_screen",["resources/audio/game-intro.mp3","resources/audio/game-intro.ogg"],{channel:"bgmusic",loop:true}]
-	]
-}
-
